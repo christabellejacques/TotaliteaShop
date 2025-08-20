385adc8c5a0d9c4d8c8894bba67e5d7f9e51ee8f
christabellejacques <christabelle.jacques@capgemini.com>
2025-08-20 08:34:39 +0100

    Moved pom file into root Added TotaliteaShopApplication as it wasn't added mannually

---
+++ src/main/java/com/totalitea/TotaliteaShop/TotaliteaShopApplication.java
@@ -0,0 +1,13 @@
+package com.totalitea.TotaliteaShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotaliteaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotaliteaShopApplication.class, args);
+    }
+
+}
